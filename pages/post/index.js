f6fe27ba714914f9f008b0c2cf3b1f9c0396fc3b
João Thiago Souza Silva <106702402+joaoThiagoSSilva@users.noreply.github.com>
2022-10-31 09:57:04 -0300

    styles add

--- pages/post/index.js
+++
@@ -1,8 +0,0 @@
-import { homeButton, renderButtonsHome } from "../../scripts/buttons.js";
-import { mainPost } from "../../scripts/mainPost.js";
-
-homeButton()
-
-await mainPost(localStorage.getItem("postId"))
-
-renderButtonsHome("btns-wrapper-post")
