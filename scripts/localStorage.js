7639511e69f22448e422fa876c33248210cdb26c
João Thiago Souza Silva <106702402+joaoThiagoSSilva@users.noreply.github.com>
2022-10-31 09:42:03 -0300

    Delete scripts directory

--- scripts/localStorage.js
+++
@@ -1,7 +0,0 @@
-export const setInitialCategoryLocalStorage = () => {
-  const categoryNow = localStorage.getItem("categoryNow")
-
-  if (!categoryNow) {
-    localStorage.setItem("categoryNow", "Todos")
-  }
-}
